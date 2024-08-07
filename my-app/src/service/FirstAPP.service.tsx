c90085109da423a3807aa5785ffbb5135fe33943
shivtcs1983 <shivtcs1983@gmail.com>
2024-08-07 12:03:04 -0400

    my-initial-chnages

---
+++ my-app/src/service/FirstAPP.service.tsx
@@ -0,0 +1,14 @@
+
+import axios from 'axios'
+export async function myTestData(){
+
+    const endPointUrl= ''+'TestData';
+
+    let serviceResponse;
+    try{
+        serviceResponse = "Stay tuned...";// await axios.get(endPointUrl) ;
+    }catch(error){
+        console.log('---error--', error);
+    }
+    return serviceResponse
+}
